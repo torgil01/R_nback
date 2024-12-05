84f7bc51815087dafdc49d4c5acfe3e355d601d0
torgil01 <torgil.vangberg@gmail.com>
2024-12-05 16:41:40 +0100

    added loop for reading all dat files in dir

--- convert_dat.R
+++ convert_dat.R
@@ -1,8 +1,48 @@
-convert_dat <- function(filename) {
+convert_dat <- function(data_dir) {
   source(here::here("gen_df.R"))
   source(here::here("read_dat.R"))
+  source(here::here("get_empty_tibble.R"))
 
-  df.raw <- read_dat(filename)
-  df <- gen_df(df.raw)
+
+# local functions
+  
+  get_id <- function(fname) {
+    # extract id from filename
+    fn <- basename(fname)
+    # filename is like
+    # id_xxxx
+    pos <- str_locate(fn,"_")  
+    id <- substring(fn,1,pos[1]-1)  
+    return(id)
+  }
+
+
+
+  # ------ MAIN ------------
+  # allocate the final df
+  df <- get_empty_tibble()
+  
+  data_files <- list.files(data_dir, pattern = ".log", full.names =TRUE)
+  for (i in 1:length(data_files)) {
+    cat("parsing ", data_files[i], "\n")
+    df.raw <- read_dat(data_files[i])  
+    # check if thest was completed 
+    if (any(str_detect(df.raw$desc,"text_13: text = 'Oppgaven er ferdig.") == TRUE)) {
+      # complete test 
+      df.id <- gen_df(df.raw)
+      id <- get_id(data_files[i])
+      cat("id = ", id, "\n")
+      df.row <- convert_to_row(id, df.id)
+      cat(df.row$id[1])
+      df <- df %>% add_row(df.row)
+    }
+    else {
+      cat( data_files[i], "is incomplete .. skipping", "\n")      
+    }        
+  }
+
+
+  return(df)
+  
 
 }
