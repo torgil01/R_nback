95bf412bb3d62dd6601974b4c6f4dea6dc648054
torgil01 <torgil.vangberg@gmail.com>
2024-12-10 19:12:00 +0100

    first commit

---
+++ convert_dat_csv.R
@@ -0,0 +1,39 @@
+convert_dat_csv <- function(data_dir) {
+  # libs
+  library(dplyr)
+
+  # funs
+  source(here::here("get_empty_tibble2.R"))
+  source(here::here("summarize_trial.R"))
+  source(here::here("convert_csv.R"))
+
+
+
+
+  # ------ MAIN ------------
+  
+  csv_files <- list.files(data_dir, pattern = ".csv", full.names =TRUE)
+  
+  # allocate the final df
+  df <- get_empty_tibble2(length(csv_files))
+  
+  for (i in 1:length(csv_files)) {
+    cat("parsing ", csv_files[i], "\n")
+    df.csv <- convert_csv(csv_files[i])  
+    # check if the test was completed
+    if (dim(df.csv)[1] == 150) {
+      # ok 
+      cat("test is complete: ", csv_files[i], "\n")
+      df.row <- summarize_trial(df.csv)
+      df <- df %>% add_row(df.row)
+    }
+    else {
+      cat(csv_files[i], "is incomplete .. skipping", "\n")      
+    }       
+  } 
+  df$test_date <- as.Date(df$test_date, format ="%Y-%m-%d")
+  df <- df %>% filter(id != "")
+  return(df)
+  
+
+}
---
+++ get_empty_tibble2.R
@@ -0,0 +1,44 @@
+get_empty_tibble2 <- function(n) {
+  library(dplyr)
+  df <- data.frame(
+    id = character(n),
+    trial = character(n),
+    test_date = character(n),
+    test_time = character(n),
+    filename = character(n),
+    num_correct_0b = integer(n),
+    num_correct_2b = integer(n),
+    num_errors_0b = integer(n),
+    num_errors_2b = integer(n),
+    num_miss_0b = integer(n),
+    num_miss_2b = integer(n),
+    rt_correct_0b  = double(n),
+    rt_correct_2b = double(n),
+    rt_errors_0b  = double(n),
+    rt_errors_2b  = double(n),
+    std_correct_0b = double(n),
+    std_correct_2b = double(n),
+    std_errors_0b = double(n),
+    std_errors_2b = double(n)    
+  ) 
+
+
+  #
+  # num_correct_0b # either correct keypress or no reposne 
+  # num_correct_2b # either correct keypress or no reposne 
+  # num_errors_0b  # keypress when there should be none
+  # num_errors_2b  # keypress when there should be none
+  # num_miss_0b    # no kreypress when there should be one 
+  # num_miss_2b    # no kreypress when there should be one 
+  # rt_correct_0b  # mean rt for correct keypress
+  # rt_correct_2b  # mean rt for correct keypress 
+  # rt_errors_0b   # mean rt for incorrect keypress
+  # rt_errors_2b   # mean rt for incorrect keypress
+  # std_correct_0b # std rt for correct keypress
+  # std_correct_2b # std rt for correct keypress
+  # std_errors_0b  # std rt for incorrect keypress
+  # std_errors_2b  # std rt for incorrect keypress 
+
+  return(df)      
+}
+
